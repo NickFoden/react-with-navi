b20fed934ff2d54894ddb32ce80e9933959ce1c0
Nick Foden <nickfoden@gmail.com>
2018-12-14 07:04:26 -0500

    Add static rendering and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-and then added Navi
+and then added [Navi](https://github.com/frontarm/navi)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { createBrowserNavigation } from "navi";
+import * as Navi from "navi";
+import React from "react";
+import ReactDOM from "react-dom";
 import pages from "./pages";
 import App from "./App";
 
-async function main() {
-  let navigation = createBrowserNavigation({ pages });
+Navi.app({
+  // Specify the pages that navi-app should statically build, by passing
+  // in a Switch object
+  pages,
 
-  // Wait until async content is ready, or has failed.
-  await navigation.steady();
+  // The default create-react-app renderer needs access to the App
+  // component.
+  exports: App,
 
-  ReactDOM.render(
-    <App navigation={navigation} />,
-    document.getElementById("root")
-  );
-}
+  async main() {
+    let navigation = Navi.createBrowserNavigation({
+      pages
+    });
 
-// Start the app
-main();
+    // Wait until the navigation has loaded the page's content,
+    // or failed to do so. If you want to load other data in parallel
+    // while the initial page is loading, make sure to start loading
+    // before this line.
+    await navigation.steady();
+
+    // React requires that you call `ReactDOM.hydrate` if there is
+    // statically rendered content in the root element, but prefers
+    // us to call `ReactDOM.render` when it is empty.
+    let hasStaticContent = process.env.NODE_ENV === "production";
+    let renderer = hasStaticContent ? ReactDOM.hydrate : ReactDOM.render;
+
+    // Start react.
+    renderer(<App navigation={navigation} />, document.getElementById("root"));
+  }
+});
